ad837665e475248523007aec49de03e0b9f50a61
RopFoo <ropfoo@gmail.com>
2021-06-24 20:34:11 +0200

    optimized random note generation

--- src/hooks/useRandomNote.ts
+++ src/hooks/useRandomNote.ts
@@ -2,11 +2,9 @@ import {useState} from 'react';
 import {note} from '../components/Quiz/_data/notes';
 
 export function useRandomNote(notes: note[]) {
-  const [currentNote, setCurrentNote] = useState<note>(notes[0]);
-  const generateRandomNote = () => {
-    const randomNumber = Math.floor(Math.random() * notes.length);
-    setCurrentNote(notes[randomNumber]);
-  };
+  const randomNumber = (): number => Math.floor(Math.random() * notes.length);
+  const [currentNote, setCurrentNote] = useState<note>(notes[randomNumber()]);
+  const generateRandomNote = () => setCurrentNote(notes[randomNumber()]);
   return {
     generateRandomNote,
     currentNote,
